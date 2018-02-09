bbc9df8640455454c5ea900540e00664afd43e86
Christian Salway <ccsalway@yahoo.co.uk>
2018-02-09 14:15:16 +0000

    updates

--- build/build_common.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#Set build number
-sed -i "s/DEVELOPMENT_BUILD/$BUILD_NUMBER/" setup.py
-
-#Build in Docker
-cd build
-chmod +x build-docker.sh
-./build-docker.sh $BBUSER $BBPASS
